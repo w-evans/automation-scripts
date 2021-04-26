a0e568d330c194c949bcb7d139fa03b7127c1f49
w-evans <wle831@gmail.com>
2021-04-26 11:22:21 -0700

    fixes

--- add_printers.ps1
+++ add_printers.ps1
@@ -22,22 +22,24 @@ $BroDriver = 'Brother Generic Jpeg Type2 Class Driver'
 $HPDriver = 'HP Universal Printing PCL 6'
 
 # INITIALIZE/SET PRINTER VALUES ###########
-$RedPrinter = [PrinterTempl]::new(); $RedPrinter.PrinterName = 'PT-Red_Printer'; $RedPrinter.PrinterPort = 'PT-Red_Printer'; $RedPrinter.PrinterIP = '192.168.26.XXX'; $RedPrinter.PrinterDriver = $BroDriver
-$BluePrinter = [PrinterTempl]::new(); $BluePrinter.PrinterName = 'PT-Blue_Printer'; $BluePrinter.PrinterPort = 'PT-Blue_Printer'; $BluePrinter.PrinterIP = '192.168.26.XXX'; $BluePrinter.PrinterDriver = $BroDriver
-$OrangePrinter = [PrinterTempl]::new(); $OrangePrinter.PrinterName = 'PT-Orange_Printer'; $OrangePrinter.PrinterPort = 'PT-Orange_Printer'; $OrangePrinter.PrinterIP = '192.168.26.XXX'; $OrangePrinter.PrinterDriver = $BroDriver
-$YellowPrinter = [PrinterTempl]::new();$Yellowrinter.PrinterName = 'PT-Yellow_Printer'; $YellowPrinter.PrinterPort = 'PT-Yellow_Printer'; $YellowPrinter.PrinterIP = '192.168.26.XXX'; $YellowPrinter.PrinterDriver = $BroDriver
-$RainbowPrinter = [PrinterTempl]::new(); $RainbowPrinter.PrinterName = 'PT-Rainbow_Printer'; $RainbowPrinter.PrinterPort = 'PT-Rainbow_Printer'; $RainbowPrinter.PrinterIP = '192.168.26.XXX'; $RainbowPrinter.PrinterDriver = $BroDriver
-$BlackPrinter = [PrinterTempl]::new(); $BlackPrinter.PrinterName = 'PT-Black_Printer'; $BlackPrinter.PrinterPort = 'PT-Black_Printer'; $BlackPrinter.PrinterIP = '192.168.26.XXX'; $BlackPrinter.PrinterDriver = $BroDriver
-$PurplePrinter = [PrinterTempl]::new(); $PurplePrinter.PrinterName = 'PT-Purple_Printer'; $PurplePrinter.PrinterPort = 'PT-Purple_Printer'; $PurplePrinter.PrinterIP = '192.168.26.XXX'; $PurplePrinter.PrinterDriver = $BroDriver
-$GrayPrinter = [PrinterTempl]::new(); $GrayPrinter.PrinterName = 'PT-Gray_Printer'; $GrayPrinter.PrinterPort = 'PT-Gray_Printer'; $GrayPrinter.PrinterIP = '192.168.26.XXX'; $GrayPrinter.PrinterDriver = $BroDriver
+$RedPrinter = [PrinterTempl]::new(); $RedPrinter.PrinterName = 'PT-Red_Printer'; $RedPrinter.PrinterPort = 'PT-Red_Printer'; $RedPrinter.PrinterIP = '192.168.26.5'; $RedPrinter.PrinterDriver = $BroDriver
+$BluePrinter = [PrinterTempl]::new(); $BluePrinter.PrinterName = 'PT-Blue_Printer'; $BluePrinter.PrinterPort = 'PT-Blue_Printer'; $BluePrinter.PrinterIP = '192.168.26.6'; $BluePrinter.PrinterDriver = $BroDriver
+$OrangePrinter = [PrinterTempl]::new(); $OrangePrinter.PrinterName = 'PT-Orange_Printer'; $OrangePrinter.PrinterPort = 'PT-Orange_Printer'; $OrangePrinter.PrinterIP = '192.168.26.7'; $OrangePrinter.PrinterDriver = $BroDriver
+$RainbowPrinter = [PrinterTempl]::new(); $RainbowPrinter.PrinterName = 'PT-Rainbow_Printer'; $RainbowPrinter.PrinterPort = 'PT-Rainbow_Printer'; $RainbowPrinter.PrinterIP = '192.168.26.8'; $RainbowPrinter.PrinterDriver = $BroDriver
+$BlackPrinter = [PrinterTempl]::new(); $BlackPrinter.PrinterName = 'PT-Black_Printer'; $BlackPrinter.PrinterPort = 'PT-Black_Printer'; $BlackPrinter.PrinterIP = '192.168.26.9'; $BlackPrinter.PrinterDriver = $BroDriver
+$PurplePrinter = [PrinterTempl]::new(); $PurplePrinter.PrinterName = 'PT-Purple_Printer'; $PurplePrinter.PrinterPort = 'PT-Purple_Printer'; $PurplePrinter.PrinterIP = '192.168.26.10'; $PurplePrinter.PrinterDriver = $BroDriver
+$GrayPrinter = [PrinterTempl]::new(); $GrayPrinter.PrinterName = 'PT-Gray_Printer'; $GrayPrinter.PrinterPort = 'PT-Gray_Printer'; $GrayPrinter.PrinterIP = '192.168.26.11'; $GrayPrinter.PrinterDriver = $BroDriver
+$YellowPrinter = [PrinterTempl]::new(); $YellowPrinter.PrinterName = 'PT-Yellow_Printer'; $YellowPrinter.PrinterPort = 'PT-Yellow_Printer'; $YellowPrinter.PrinterIP = '192.168.26.12'; $YellowPrinter.PrinterDriver = $BroDriver
 
 # Place All Initilized Printer Instanced objects into an Array List
 $PrinterArray = $RedPrinter, $BluePrinter, $OrangePrinter, $YellowPrinter, $RainbowPrinter, $BlackPrinter, $PurplePrinter, $GrayPrinter
 
 # ITERATE THROUGH OUR PRINTER ARRAY TO ADD PRINTERS ON LOCAL MACHINE
-for ($num = 0, $num -le $PrinterArray.length() - 1 ; $num++) {
-Add-PrinterPort -Name $PrinterArray[$num].PrinterName -PrinterHostAddress $PrinterArray[$num].PrinterIP && Start-Sleep 1
-Add-PrinterDriver -Name $PrinterArray[$num].PrinterDriver && Start-Sleep 1
+for ($num = 0; $num -le 8; $num++) {
+Add-PrinterPort -Name $PrinterArray[$num].PrinterName -PrinterHostAddress $PrinterArray[$num].PrinterIP 
+Start-Sleep 1
+Add-PrinterDriver -Name $PrinterArray[$num].PrinterDriver 
+Start-Sleep 1
 Add-Printer -Name $PrinterArray[$num].PrinterName -DriverName $PrinterArray[$num].PrinterDriver -Port $PrinterArray[$num].PrinterPort
 }
 
