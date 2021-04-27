0e2ccf9dc7d99785ed5fda99b6539ea3442cd69d
Will Evans <wle831@gmail.com>
2021-04-27 10:20:35 -0700

    syntax fix

--- add_VPN.ps1
+++ add_VPN.ps1
@@ -1,9 +1,5 @@
 $VPNaddress = 'pt-brwqkwngtbc.dynamic-m.com'
-<<<<<<< HEAD
 $PreSharedKey = Read-Host 'enter pre-shared key' -MaskInput -AsSecureString
-=======
-$PreSharedKey = ''
->>>>>>> caf1fe67dfe77d3b53656f07f6682b813ffb5f6a
 $VPNname = 'PT-VPN'
 $VPNtype = 'L2tp'
 
