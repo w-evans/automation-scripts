e086f8b94b576896b3f0e1f5e4ad92096e1fdde0
Will Evans <wle831@gmail.com>
2021-04-27 14:44:40 -0700

    execution policy update

--- add_VPN.ps1
+++ add_VPN.ps1
@@ -1,3 +1,6 @@
+#allow scripts to be run
+SetExecution-Policy Unrestricted -Force
+
 $VPNaddress = 'pt-brwqkwngtbc.dynamic-m.com'
 $PreSharedKey = Read-Host "enter Pre-Shared Key: "
 $VPNname = 'PT-VPN'
@@ -11,6 +14,9 @@ Get-VpnConnection -AllUserConnection | Remove-VpnConnection -Force
 
 #Add our VPN connection, will prompt user for PSK
 Add-VpnConnection -Name $VPNname -ServerAddress $VPNaddress -TunnelType $VPNtype -AllUserConnection -L2tpPsk $PreSharedKey -AuthenticationMethod Pap -Force
+
+#set our execution policy back to signed certs
+SetExecution-Policy AllSigned -Force
 
 #Verify that our VPN has been added
 Get-VpnConnection -AllUserConnection
