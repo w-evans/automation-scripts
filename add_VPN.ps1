f8d94a62b6950e9f4edc45b55f4a5d27fdb451e4
local.admin <wle831@gmail.com>
2021-04-27 10:45:54 -0700

    fixes

--- add_VPN.ps1
+++ add_VPN.ps1
@@ -1,10 +1,9 @@
 $VPNaddress = 'pt-brwqkwngtbc.dynamic-m.com'
-$PreSharedKey = Read-Host 'enter pre-shared key' -MaskInput -AsSecureString
+$PreSharedKey = Read-Host "enter Pre-Shared Key: "
 $VPNname = 'PT-VPN'
 $VPNtype = 'L2tp'
-
 
 Add-VpnConnection -Name $VPNname -ServerAddress $VPNaddress -TunnelType $VPNtype -AllUserConnection -L2tpPsk $PreSharedKey -AuthenticationMethod Pap -Force
 
 
-Get-VpnConnection
+Get-VpnConnection -AllUserConnection
