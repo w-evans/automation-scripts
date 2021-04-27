edc6db68bcf987c80c00ef9880cb93b4cbd36c4e
Will Evans <wle831@gmail.com>
2021-04-27 10:52:40 -0700

    fixes

--- add_VPN.ps1
+++ add_VPN.ps1
@@ -3,7 +3,11 @@ $PreSharedKey = Read-Host "enter Pre-Shared Key: "
 $VPNname = 'PT-VPN'
 $VPNtype = 'L2tp'
 
+#Remove ALL existing VPNs
+Get-VpnConnection -AllUserConnection | Remove-VpnConnection -Force
+
+#Add our VPN connection, will prompt user for PSK
 Add-VpnConnection -Name $VPNname -ServerAddress $VPNaddress -TunnelType $VPNtype -AllUserConnection -L2tpPsk $PreSharedKey -AuthenticationMethod Pap -Force
 
-
+#Verify that our VPN has been added
 Get-VpnConnection -AllUserConnection
