79bb7c1adf15f99e92eb0dfb056a0a9323c32b75
Will Evans <wle831@gmail.com>
2021-04-27 09:41:44 -0700

    vpn script testing

--- add_VPN.ps1
+++ add_VPN.ps1
@@ -1,1 +1,9 @@
+$VPNaddress = 'pt-brwqkwngtbc.dynamic-m.com'
+$PreSharedKey = 'D!gitalRail5'
+$VPNname = 'PT-VPN'
+$VPNtype = 'L2tp'
+
+
+Add-VpnConnection -Name $VPNname -ServerAddress $VPNaddress -TunnelType $VPNtype -AllUserConnection true -L2tpPs2 $PreSharedKey -AuthenticationMethod Pap -Force
+
 
