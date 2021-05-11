38d5decef44147f415e51a13e856f54643bd3bc3
Will Evans <wle831@gmail.com>
2021-05-11 08:11:14 -0700

    removed a line no longer needed

--- add_VPN-Win10.ps1
+++ add_VPN-Win10.ps1
@@ -9,8 +9,5 @@ Get-VpnConnection -AllUserConnection | Remove-VpnConnection -Force
 #Add our VPN connection, will prompt user for PSK
 Add-VpnConnection -Name $VPNname -ServerAddress $VPNaddress -TunnelType $VPNtype -AllUserConnection -L2tpPsk $PreSharedKey -AuthenticationMethod Pap -Force
 
-#set our execution policy back to signed certs
-Set-ExecutionPolicy AllSigned -Force
-
 #Verify that our VPN has been added
 Get-VpnConnection -AllUserConnection
