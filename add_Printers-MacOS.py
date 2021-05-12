9d5fb4b9dabc2cbd5d8061e2eea145a3cfe6fd44
Will Evans <wle831@gmail.com>
2021-05-12 10:15:11 -0700

    init

--- add_Printers-MacOS.py
+++ add_Printers-MacOS.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python
+#Will's fancy printer-add script
+#Copyright © 2021 PrimeTrust LLC
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without 
+#limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#PRINTER OBJECT CONSTRUCTOR
 class PrinterTempl:  
     def __init__(self, printer_name, printer_ip, printer_port, printer_driver):  
         self.printer_name = printer_name
@@ -5,15 +16,23 @@ class PrinterTempl:
         self.printer_port = printer_port
         self.printer_driver = printer_driver
 
-    def display(self):  
-        print("ID: %s \nName: %s \nDriver: %s \nPort: %s" % (self.printer_ip, self.printer_name, self.printer_driver, self.printer_port))  
-
-
-
+#LOCALLY INSTALLED DRIVERS
 bro_driver = "Universal Brother Jepg Image type 2"
+hp_drivers = "HP Universal PCL 6 Driver"
 
-red_printer = PrinterTempl("PT-RedPrinter", "192.168.26.10", "PT-RedPrinter", bro_driver)  
-  
-# accessing display() method to print employee 1 information  
-  
-red_printer.display()   
+#CREATING OUR PRINTERTEMP OBJECTS
+red_printer = PrinterTempl("PT-RedPrinter", "192.168.26.10", "PT-RedPrinter", bro_driver)
+blue_printer = PrinterTempl("PT-BluePrinter", "192.168.26.10", "PT-BluePrinter", bro_driver) 
+orange_printer = PrinterTempl("PT-OrangePrinter", "192.168.26.10", "PT-OrangePrinter", bro_driver)  
+rainbow_printer = PrinterTempl("PT-RainbowPrinter", "192.168.26.10", "PT-RainbowPrinter", bro_driver)  
+black_printer = PrinterTempl("PT-BlackPrinter", "192.168.26.10", "PT-BlackPrinter", bro_driver)  
+purple_printer = PrinterTempl("PT-PurplePrinter", "192.168.26.10", "PT-PurplePrinter", bro_driver)  
+gray_printer = PrinterTempl("PT-GrayPrinter", "192.168.26.10", "PT-GrayPrinter", bro_driver)  
+yellow_printer = PrinterTempl("PT-YellowPrinter", "192.168.26.10", "PT-YellowPrinter", bro_driver)  
+
+#PLACE ALL OUR PRINTER OBJECTS INTO AN ARRAY
+printer_array = [red_printer, blue_printer, orange_printer, rainbow_printer, black_printer, purple_printer, gray_printer, yellow_printer]
+
+#ITERATE THROUGH OUR PRINTER ARRAY AND ADD THE PRINTERS TO THE LOCAL MACHINE
+for i in printer_array:
+    print(i.printer_name)
