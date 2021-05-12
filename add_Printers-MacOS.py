725efe4a580c60c4b4c71159feaf4fc195cf4819
Will Evans <wle831@gmail.com>
2021-05-12 11:21:24 -0700

    mac script working

--- add_Printers-MacOS.py
+++ add_Printers-MacOS.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
+import os
+import time
 #Will's fancy printer-add script
-#Copyright © 2021 PrimeTrust LLC
+#Copyright 2021 PrimeTrust LLC
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without 
 #limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -20,19 +22,20 @@ class PrinterTempl:
 bro_driver = "Universal Brother Jepg Image type 2"
 hp_drivers = "HP Universal PCL 6 Driver"
 
-#CREATING OUR PRINTERTEMP OBJECTS
-red_printer = PrinterTempl("PT-RedPrinter", "192.168.26.10", "PT-RedPrinter", bro_driver)
-blue_printer = PrinterTempl("PT-BluePrinter", "192.168.26.10", "PT-BluePrinter", bro_driver) 
-orange_printer = PrinterTempl("PT-OrangePrinter", "192.168.26.10", "PT-OrangePrinter", bro_driver)  
-rainbow_printer = PrinterTempl("PT-RainbowPrinter", "192.168.26.10", "PT-RainbowPrinter", bro_driver)  
-black_printer = PrinterTempl("PT-BlackPrinter", "192.168.26.10", "PT-BlackPrinter", bro_driver)  
-purple_printer = PrinterTempl("PT-PurplePrinter", "192.168.26.10", "PT-PurplePrinter", bro_driver)  
-gray_printer = PrinterTempl("PT-GrayPrinter", "192.168.26.10", "PT-GrayPrinter", bro_driver)  
-yellow_printer = PrinterTempl("PT-YellowPrinter", "192.168.26.10", "PT-YellowPrinter", bro_driver)  
+#CREATING OUR PRINTERTEMPL OBJECTS
+red_printer = PrinterTempl("PT-RedPrinter", "192.168.100.8", "PT-RedPrinter", bro_driver)
+blue_printer = PrinterTempl("PT-BluePrinter", "192.168.100.23", "PT-BluePrinter", bro_driver) 
+orange_printer = PrinterTempl("PT-OrangePrinter", "192.168.100.28", "PT-OrangePrinter", bro_driver)  
+rainbow_printer = PrinterTempl("PT-RainbowPrinter", "192.168.26.13", "PT-RainbowPrinter", bro_driver)  
+black_printer = PrinterTempl("PT-BlackPrinter", "192.168.26.14", "PT-BlackPrinter", bro_driver)  
+purple_printer = PrinterTempl("PT-PurplePrinter", "192.168.26.15", "PT-PurplePrinter", bro_driver)  
+gray_printer = PrinterTempl("PT-GrayPrinter", "192.168.26.16", "PT-GrayPrinter", bro_driver)  
+yellow_printer = PrinterTempl("PT-YellowPrinter", "192.168.26.17", "PT-YellowPrinter", bro_driver)  
 
 #PLACE ALL OUR PRINTER OBJECTS INTO AN ARRAY
 printer_array = [red_printer, blue_printer, orange_printer, rainbow_printer, black_printer, purple_printer, gray_printer, yellow_printer]
 
 #ITERATE THROUGH OUR PRINTER ARRAY AND ADD THE PRINTERS TO THE LOCAL MACHINE
 for i in printer_array:
-    print(i.printer_name)
+    os.system("lpadmin -p " + i.printer_name + " -D " + i.printer_port + " -E -v" + " ipp://" + i.printer_ip + "/ipp/print" + " -m everywhere")
+    time.sleep(0.5)
