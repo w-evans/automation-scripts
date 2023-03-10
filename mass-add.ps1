399975f7c3ba69202c6a8490a215b2012e07cb86
Will Evans <wle831@gmail.com>
2023-03-10 13:08:29 -0800

    quick
    quick, not done

--- mass-add.ps1
+++ mass-add.ps1
@@ -1,8 +1,10 @@
 function Mass-Add {
     #any amount of users to any amount of groups in any amount of domains
+    $exceldata = Import-CSV "C:\Users.csv"
+    
     $users = Import-CSV 'C:\Users.csv' | Select -ExpandProperty userID
-    $domains = 'domain1', 'domain2', 'domain3', 'domain4', 'domain5'
-    $groups = 'group1', 'group2', 'group3', 'group4', 'group5'
+    $domains = Import-CSV 'C:\Users.csv' | Select -ExpandProperty domain
+    $groups = Import-CSV 'C:\Users.csv' | Select -ExpandProperty group
     
     ForEach ($user in $users) {  
         ForEach ($domain in $domains) {
